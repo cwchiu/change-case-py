3755abfd17fea9067382b6be13fea2064da5875e
Chui-Wen Chiu <sisimi.pchome@gmail.com>
2020-07-20 11:40:55 +0800

    Create __init__.py

---
+++ pychangecase/__init__.py
@@ -0,0 +1,1 @@
+from .pychangecase import pascal_case, camel_case, snake_case, capital_case, constant_case, dot_case, header_case, param_case, path_case, sentence_case, swap_case, upper_case_fist, upper_case, lower_case_fist, lower_case
